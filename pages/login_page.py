eba6933042e222b3357113226c762689a01e71ae
oxoexe <oxo.exe@yandex.ru>
2022-03-05 10:27:30 +0300

    инизиализован LoginPage в теле теста

---
+++ pages/login_page.py
@@ -0,0 +1,26 @@
+from .base_page import BasePage
+from .locators import LoginPageLocators
+
+
+class LoginPage(BasePage):
+    def should_be_login_page(self):
+        self.should_be_login_url()
+        self.should_be_login_form()
+        self.should_be_register_form()
+
+    def should_be_login_url(self):
+        # реализуйте проверку на корректный url адрес
+        current_url = self.browser.current_url
+        print(current_url)
+        assert LoginPageLocators.LOGIN_URL in current_url != False, "не правильный url"
+        #
+
+    def should_be_login_form(self):
+        # реализуйте проверку, что есть форма логина
+        assert self.is_element_present(*LoginPageLocators.LOGIN_FORM), "форма логина не найдена"
+        assert True
+
+    def should_be_register_form(self):
+        # реализуйте проверку, что есть форма регистрации на странице
+        assert self.is_element_present(*LoginPageLocators.REG_FORM), "форма регистрации не найдена"
+        assert True
--- test_main_page.py
+++ test_main_page.py
@@ -1,10 +1,13 @@
 from .pages.main_page import MainPage
-
+from .pages.login_page import LoginPage
 
 
 def test_guest_can_go_to_login_page(browser):
-    link = "http://selenium1py.pythonanywhere.com/"
+    link = "http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209?promo=midsummer "
     page = MainPage(browser, link)   # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес
     page.open()                      # открываем страницу
+    page.should_be_login_link()      # проверяем наличие ссылки
     page.go_to_login_page()          # выполняем метод страницы — переходим на страницу логина
-    page.should_be_login_link()
+    login_page = LoginPage(browser, browser.current_url) # инициализируем LoginPage
+    login_page.should_be_login_page() # запуск метода LoginPage для проверки url, login формы и register формы
+
