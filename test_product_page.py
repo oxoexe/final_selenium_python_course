d6667aae10972ee0b271b513fd91e1c219bcee31
oxoexe <oxo.exe@yandex.ru>
2022-03-14 18:27:28 +0300

    добавлен тестовый класс для реализации методов регистрация / добавление товара / проверка добавления в корзину

--- test_product_page.py
+++ test_product_page.py
@@ -1,52 +1,72 @@
-# import pytest # был добавлен для решения задания с xfail
+import pytest
 from .pages.product_page import ProductPage
 from .pages.basket_page import BasePage
+from .pages.login_page import LoginPage
 import time
 
 
-# @pytest.mark.parametrize('link', ["http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer0",
-#                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer1",
-#                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer2",
-#                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer3",
-#                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer4",
-#                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer5",
-#                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer6",
-#                                  pytest.param("http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer7", marks=pytest.mark.xfail(reason="fixing this bug right now")), # Отметили с помощью xfail падающий тест
-#                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer8",
-#                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer9"])
-# def test_guest_can_add_product_to_basket(browser, link): # второй аргумент link здесь для примера с параметризацией
-def test_guest_can_add_product_to_basket(browser):
-    # link = "http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209/?promo=newYear"
-    link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=newYear2019"
-    # link = "http://selenium1py.pythonanywhere.com/ru/catalogue/coders-at-work_207/?promo=offer0"
-    product_page = ProductPage(browser, link)
-    product_page.open()
-    product_page.add_to_cart()
-    product_page.solve_quiz_and_get_code()
-    # product_page.checking_add_to_cart_message(product_page.product_name())
-    # product_page.price_check_after_adding_to_kart(product_page.product_price())
-    # product_page.should_dissapear()
-    # time.sleep(30)
+@pytest.mark.login
+class TestUserAddToBasketFromProductPage():
+    @pytest.fixture(scope="function", autouse=True)
+    def setup(self, browser):
+        link = "http://selenium1py.pythonanywhere.com/ru/accounts/login/"
+        page = LoginPage(browser, link)
+        page.open()
+        page.register_new_user()
+        page.should_be_authorized_user()
+
+    def test_user_cant_see_success_message(self, browser):
+        link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=newYear2019"
+        page = ProductPage(browser, link)
+        page.open()
+        page.should_not_be_success_message()
+
+    def test_user_can_add_product_to_basket(self, browser):
+        link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=newYear2019"
+        page = ProductPage(browser, link)
+        page.open()
+        page.add_to_cart()
+        page.solve_quiz_and_get_code()
+        page.checking_add_to_cart_message(page.product_name())
 
 
-def test_guest_should_see_login_link_on_product_page(browser):
-    link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
-    page = ProductPage(browser, link)
-    page.open()
-    page.should_be_login_link()
+@pytest.mark.login_guest
+@pytest.mark.parametrize('link', ["http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer0",
+                                 "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer1",
+                                 "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer2",
+                                 "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer3",
+                                 "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer4",
+                                 "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer5",
+                                 "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer6",
+                                 pytest.param("http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer7", marks=pytest.mark.xfail(reason="fixing this bug right now")), # Отметили с помощью xfail падающий тест
+                                 "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer8",
+                                 "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer9"])
+class TestGuestAddToBasketFromProductPage():
+    def test_guest_can_add_product_to_basket(self, browser, link): # так как используем параметризацию передаем link
+        #link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=newYear2019"
+        page = ProductPage(browser, link)
+        page.open()
+        page.add_to_cart()
+        page.solve_quiz_and_get_code()
+        page.checking_add_to_cart_message(page.product_name())
 
-def test_quest_can_go_to_login_page_from_produt_page(browser):
-    link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
-    page = ProductPage(browser, link)
-    page.open()
-    page.go_to_login_page()
+    def test_guest_cant_see_product_in_basket_opened_from_product_page(self, browser, link):
+        page = ProductPage(browser, link)
+        page.open()
+        page.go_to_basket() # переход в корзину из шапки
+        page = BasePage(browser, browser.current_url)
+        page.shold_be_empty_basket_text()
+        page.should_be_emty_basket()
 
-def test_guest_cant_see_product_in_basket_opened_from_product_page(browser):
-    link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/studyguide-for-counter-hack-reloaded_205/"
-    page = ProductPage(browser, link)
-    page.open()
-    page.go_to_basket() # переход в корзину из шапки
-    basket_page = BasePage(browser, browser.current_url)
-    basket_page.shold_be_empty_basket_text()
-    basket_page.should_be_emty_basket()
+    def test_guest_should_see_login_link_on_product_page(self, browser, link):
+        page = ProductPage(browser, link)
+        page.open()
+        page.should_be_login_link()
+
+    def test_guest_can_go_to_login_page_from_produt_page(self, browser, link):
+        page = ProductPage(browser, link)
+        page.open()
+        page.go_to_login_page()
+
+
 
