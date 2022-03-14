76d84450425ac59edfd85ff1322395c3af245a81
oxoexe <oxo.exe@yandex.ru>
2022-03-14 17:56:20 +0300

    добавлен метода регистрации пользователя register_new_user

--- pages/login_page.py
+++ pages/login_page.py
@@ -1,6 +1,6 @@
 from .base_page import BasePage
 from .locators import LoginPageLocators
-
+import time
 
 class LoginPage(BasePage):
     def should_be_login_page(self):
@@ -24,3 +24,18 @@ class LoginPage(BasePage):
         # реализуйте проверку, что есть форма регистрации на странице
         assert self.is_element_present(*LoginPageLocators.REG_FORM), "форма регистрации не найдена"
         assert True
+
+    def register_new_user(self):
+        email = str(time.time()) + "@fakemail.org"
+        password = str(time.time()) + "password"
+        print(email)
+        print(password)
+        input1 = self.browser.find_element(*LoginPageLocators.REGISTRATION_EMAIL)
+        input1.send_keys(email)
+        input2 = self.browser.find_element(*LoginPageLocators.REGISTRATION_PASSWORD1)
+        input2.send_keys(password)
+        input3 = self.browser.find_element(*LoginPageLocators.REGISTRATION_PASSWORD2)
+        input3.send_keys(password)
+        registration_button = self.browser.find_element(*LoginPageLocators.REGISTRATION_BUTTON)
+        registration_button.click()
+
