7cd1e81fc9196c4f719435e2d4ea7e3966e266c2
oxoexe <oxo.exe@yandex.ru>
2022-03-14 17:55:30 +0300

    добавлен локатор для реализации метода проверки авиторизации should_be_authorized_user

--- pages/locators.py
+++ pages/locators.py
@@ -9,6 +9,10 @@ class LoginPageLocators():
     LOGIN_URL = "login"
     LOGIN_FORM = (By.CSS_SELECTOR, "#login_form")
     REG_FORM = (By.CSS_SELECTOR, "#register_form")
+    REGISTRATION_EMAIL = (By.CSS_SELECTOR, '#id_registration-email')
+    REGISTRATION_PASSWORD1 = (By.CSS_SELECTOR, '#id_registration-password1')
+    REGISTRATION_PASSWORD2 = (By.CSS_SELECTOR, '#id_registration-password2')
+    REGISTRATION_BUTTON = (By.CSS_SELECTOR, "#register_form > button")
 
 class ProductPageLocators():
     ADD_TO_BUCKET_BUTTON = (By.CSS_SELECTOR, "button.btn-add-to-basket")
@@ -20,6 +24,7 @@ class ProductPageLocators():
 class BasePageLocators():
     LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
     BASKET_HEADER_LINK = (By.CSS_SELECTOR, "#default > header > div.page_inner > div > div.basket-mini.pull-right.hidden-xs > span > a")
+    USER_ICON = (By.CSS_SELECTOR, ".icon-user")
 
 class BasketPageLocators():
     EMPTY_BASKET_TEXT = (By.CSS_SELECTOR, "#content_inner > p")
